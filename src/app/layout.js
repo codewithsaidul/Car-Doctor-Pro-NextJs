2ad52add2697df170d08c483f145787a7068f828
codewithsaidul <codewithsaidul@gmail.com>
2024-12-11 22:34:58 +0600

    Mongodb connection added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import Footer from "../components/Shared/Footer";
 import Navbar from "../components/Shared/Navbar";
 import "./globals.css";
-
+import AuthProvider from "../context/AuthProvider"
 
 export const metadata = {
   title: "Car Doctor Pro || CodeWithSaidul",
@@ -12,10 +12,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" data-theme="carDoctorTheme">
       <body>
-        <Navbar />
-        {children}
-        <Footer />
-        </body>
+        <AuthProvider>
+          <Navbar />
+          {children}
+          <Footer />
+        </AuthProvider>
+      </body>
     </html>
   );
 }
--- src/components/HomePage/Services.jsx
+++ src/components/HomePage/Services.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image"
-import { CarServices } from "../../../public/services"
+import { CarServices } from "../../../lib/services"
 
 const Services = () => {
   return (
